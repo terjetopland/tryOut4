62ddd2cb1c39b4d42fcb8ba644baa0766b553a5d
Terje Topland <terjetop@uia.no>
2023-02-16 13:26:21 +0100

    can register user and login

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import * as React from 'react';
+import {useState} from "react";
+import Login from './components/Login';
+import Register from "./components/Register";
+import {Button, View, Text, StyleSheet} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+
+
+const App = () => {
+    const [user, setUser] = useState('');
+
+    if (!user) {
+        return (
+            <View style={baseStyles.overall}>
+                <View>
+                    <Register/>
+                </View>
+                <View>
+                    <Login setUser={setUser}/>
+                </View>
+            </View>
+        )}
+    else {
+        return (
+            <View style={baseStyles.overall}>
+                <Text>
+                    Now it worked fine!!
+                </Text>
+            </View>
+        );
+    }
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const baseStyles = StyleSheet.create({
+    overall: {
+        marginTop: 100,
+        textAlign: 'center',
+        backgroundColor: 'rgba(180,255,240, 0.5)'
+    }
 });
+export default App;
