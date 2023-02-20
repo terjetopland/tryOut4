e7a1a8fb11483df090178712e201aba633d28bde
Terje Topland <terjetop@uia.no>
2023-02-20 20:46:47 +0100

    Can register, login and logout. But have cycles I need to remove...?

--- components/Login.js
+++ components/Login.js
@@ -1,11 +1,10 @@
-// Component implementing a login screen
-import React, {useState, Component} from "react";
+import React, {useState} from "react";
 import {signInWithEmailAndPassword} from "firebase/auth";
 import {auth} from "../firebaseConfig";
-import {Button, Text, TextInput, View, StyleSheet} from "react-native";
-import Register from "./Register";
-import {createNativeStackNavigator} from "react-native-screens/native-stack";
+import {Button, TextInput, View, StyleSheet, Alert} from "react-native";
 import GoToButton from "./GoToButton";
+import Register from "./Register";
+
 
 
 
@@ -14,8 +13,8 @@ import GoToButton from "./GoToButton";
 // The prop setUser is a function that allows the parent component to get the logged-in user from this component
 const Login = ({setUser}) => {
     // Component state, mirrors the input fields
-    const [username, setUsername] = useState(''); // person1@person1.com
-    const [password, setPassword] = useState(''); // Password1.
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
 
     // Logs in the user based on the value of the component state.
@@ -25,6 +24,7 @@ const Login = ({setUser}) => {
             .then((userCredential) => {
                 const user = userCredential.user;
                 console.log(`User has been signed in: ${user.email}`);
+                Alert.alert('Success!', 'You are now logged in!');
 
                 // Call the setter passed to us as a prop
                 setUser(user);
@@ -55,6 +55,7 @@ const Login = ({setUser}) => {
                 onPress={loginUser}
                 title="Log in"
             />
+            <GoToButton screenName={Register}/>
         </View>
     );
 }
--- components/LoginRegisterContainer.js
+++ components/LoginRegisterContainer.js
@@ -1,21 +1,23 @@
 import React from "react";
 import { NavigationContainer} from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Logout from "./Logout";
-import Home from "./Home";
+import Login from "./Login";
+import Register from "./Register";
+
 
 const StackLoggedIn = createNativeStackNavigator();
 
-function HomeStack() {
+
+function LoginRegisterStack() {
     return (
         <NavigationContainer>
             <StackLoggedIn.Navigator>
-                <StackLoggedIn.Screen name="Home" component={Home}/>
-                <StackLoggedIn.Screen name="Logout" component={Logout}/>
+                <StackLoggedIn.Screen name="Login" component={Login}/>
+                <StackLoggedIn.Screen name="Register" component={Register}/>
             </StackLoggedIn.Navigator>
         </NavigationContainer>
     );
 }
 
 
-export default HomeStack;
+export default LoginRegisterStack;
