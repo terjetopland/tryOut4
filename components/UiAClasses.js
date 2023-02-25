4ecc6f351e9c2cea3fe1b56bf1f2e21319dad7cf
Terje Topland <terjetop@uia.no>
2023-02-25 14:33:42 +0100

    Can now get all classes

--- components/UiAClasses.js
+++ components/UiAClasses.js
@@ -1,15 +1,45 @@
-import {Button, ScrollView, View} from "react-native";
+import { ScrollView, View} from "react-native";
 import {AddClasses} from "./AddUiAClasses";
 import {GetUiAClassById} from "./GetUiAClassById";
 import { GetAllUiAClasses } from "./GetAllUiAClasses";
+import {useEffect, useState} from "react";
+import {DataTable} from "react-native-paper";
 
 
-const UiAClasses = ()   => {
+function UiAClasses(){
+    const [UiAClasses, setUiAClasses] = useState([]);
+
+    useEffect(() => {
+        const getTheClasses = async () => {
+            GetAllUiAClasses(setUiAClasses)
+        }
+        getTheClasses().then(() => {
+            console.log('Loading classes')
+        });
+        }, [])
+
+
     return (
         <ScrollView>
             <View>
                 <AddClasses/>
                 <GetUiAClassById/>
+            </View>
+            <View>
+                <DataTable>
+                    <DataTable.Header>
+                        <DataTable.Title>Class id</DataTable.Title>
+                        <DataTable.Title>Class name</DataTable.Title>
+                    </DataTable.Header>
+                    {UiAClasses.map(classes => {
+                        return (
+                            <DataTable.Row key={classes.id}>
+                                <DataTable.Cell>{classes.id}</DataTable.Cell>
+                                <DataTable.Cell>{classes.className}</DataTable.Cell>
+                            </DataTable.Row>
+                        )
+                    })}
+                </DataTable>
             </View>
         </ScrollView>
     )
